7c6bd436096a424cf1d89ddc630a5aec7cd7081e
simplerick <simplerick@yandex.com>
2023-06-28 15:42:37 +0300

    added console logger

--- dltool/log.py
+++ dltool/log.py
@@ -2,6 +2,55 @@ from collections import defaultdict
 import numpy as np
 from collections.abc import Sequence, Mapping, Callable
 import torch
+import logging
+import re
+import yaml
+
+
+class ConsoleLogger(logging.Logger):
+    color_bold = "\033[1m"
+    color_darkcyan = "\033[36m"
+    color_darkblue = "\033[34m"
+    color_end = "\033[0m"
+
+    def __init__(self, name, formatting: str = None):
+        super().__init__(name)
+        formatting = formatting or \
+            f"{ConsoleLogger.color_bold}{ConsoleLogger.color_darkblue}%(name)s{ConsoleLogger.color_end}: %(message)s"
+        formatter = logging.Formatter(
+            formatting,
+            datefmt="%d-%b-%y %H:%M:%S")
+        handler = logging.StreamHandler()
+        handler.setFormatter(formatter)
+        self.addHandler(handler)
+
+def unflatten_dict(d: dict, sep: str = "."):
+    """
+    Unflatten a dictionary with keys like "a.b.c" to a nested dictionary. Supports regex separators.
+    """
+    unflatten_d = {}
+    for k, v in d.items():
+        k_lvls = re.split(sep, k)
+        cur_d = unflatten_d
+        for l in k_lvls[:-1]:
+            if l not in cur_d:
+                cur_d[l] = {}
+            cur_d = cur_d[l]
+        cur_d[k_lvls[-1]] = v
+    return unflatten_d
+
+def format_metrics_dict_to_str(d: dict, step: int = None) -> str:
+    """
+    Formats a dictionary to a beautiful string.
+    """
+    grouped_d = {k if "/" in k else "/"+k: v for k, v in d.items()}
+    grouped_d = {k: f"{v:.4g}" for k, v in grouped_d.items()}
+    grouped_d = unflatten_dict(grouped_d, "[/.]")
+    s = [f"step {step}\n" if step is not None else "\n"]
+    for group, metrics in grouped_d.items():
+        s.append(f"{ConsoleLogger.color_darkcyan}{group}{ConsoleLogger.color_end}")
+        s.append(yaml.dump({' ': metrics} if group else metrics).replace("\'", ""))
+    return "".join(s)
 
 
 def summarize(data: Sequence[dict], function: Callable, names: Sequence[str] = None, pop: bool = False) -> dict:
@@ -75,9 +124,11 @@ class Logger:
     def __init__(self,
                  wandb_api,
                  logging_freq: int = 1,
+                 console_output: bool = True,
                  scalar_aggregation: Callable = np.mean,
                  tensor_aggregation: Callable = torch.cat):
         self.api = wandb_api
+        self.console_logger = ConsoleLogger(__name__) if console_output else None
         self.logging_freq = logging_freq
         self.scalar_aggregation = scalar_aggregation
         self.tensor_aggregation = tensor_aggregation
@@ -119,6 +170,10 @@ class Logger:
         self._data_storage = defaultdict(list)
         self._data_types = defaultdict(dict)
 
+    @staticmethod
+    def path_cat(*parts: str):
+        return "/".join([x for x in parts if x])
+
     def write(self, step: int, groups: Sequence[str] = None, metric_names: Sequence[str] = None):
         scalar_metrics, tensor_metrics = {}, {}
         for g in self._data_storage.copy():
@@ -126,11 +181,11 @@ class Logger:
                 continue
             self._last_writing_step[g] = step
             data = self._data_storage.pop(g)
-            g_str = f"{g}/" if g is not None else ""
+            g_str = str(g) if g is not None else ""
             metrics_to_write = metric_names or self._data_types[g].keys()
             # average scalar metrics
             scalar_metric_names = [m for m in metrics_to_write if self._data_types[g][m] in [float, int]]
-            scalar_metrics.update({g_str + n: m for n, m in
+            scalar_metrics.update({Logger.path_cat(g_str, n) : m for n, m in
                 summarize(data, self.scalar_aggregation, names=scalar_metric_names, pop=True).items()})
             # stack tensor metrics
             tensor_metric_names = [m for m in metrics_to_write if self._data_types[g][m] == torch.Tensor]
@@ -140,14 +195,17 @@ class Logger:
         # log scalar metrics
         if scalar_metrics:
             self.api.log(scalar_metrics, step)
+            if self.console_logger:
+                self.console_logger.info(format_metrics_dict_to_str(scalar_metrics, step))
             for n, v in scalar_metrics.items():
                 self.history[n].append(v)
         # save tensor metrics as artifact (dict with torch.tensors as wandb.Artifact)
         if tensor_metrics:
             artifact = self.api.Artifact(f"{self.api.run.id}_{step}", type='tensor')
             for g_str, metrics in tensor_metrics.items():
-                with artifact.new_file(g_str + "metrics.pt", mode='wb') as f:
+                with artifact.new_file(Logger.path_cat(g_str, "metrics.pt"), mode='wb') as f:
                     torch.save(metrics, f)
+                self.history[g_str].append(metrics)
             self.api.log_artifact(artifact)
 
     def plot(self, x_key, y_key, group=None, stroke=None, title=None):
